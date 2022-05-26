13fdcafdc606e59e3b50c784bb7751b0e57ca263
Jafeht Soturno <jasordev@gmail.com>
2022-05-25 21:50:29 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const API = "https://api.github.com/users/";
+
+
+
+const app = Vue.createApp({
+  data() {
+    return {
+      search: null,
+      result: null,
+      error: null
+    };
+  },
+  methods: {
+    async  doSearch() {
+    this.result = this.error = null
+      try {
+        const response = await fetch(API + this.search)
+        if (!response.ok) throw new Error("User not found")
+        const data = await response.json()
+        console.log(data)
+        this.result = true
+      } catch(error) {
+        this.error = error
+      } finally {
+        this.search = null
+      }
+    }
+  }
+});
