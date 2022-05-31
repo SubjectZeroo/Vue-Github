204c31a085418cce06a6229c09a52eb3f009bcb1
Jafeht Soturno <jasordev@gmail.com>
2022-05-30 23:35:08 -0400

    termiando curso hasta los momentos

--- js/main.js
+++ js/main.js
@@ -7,8 +7,25 @@ const app = Vue.createApp({
     return {
       search: null,
       result: null,
-      error: null
+      error: null,
+      favorites: new Map()
     };
+  },
+  created() {
+    const savedFavorites = JSON.parse(window.localStorage.getItem('favorites'))
+
+    if(savedFavorites.length) {
+      const favorites = new Map(savedFavorites.map(favorite => [favorite.id, favorite]))
+      this.favorites = favorites
+    }
+  },
+  computed: {
+    isFavorite() {
+      return this.favorites.has(this.result.id)
+    },
+    allFavorites(){
+      return Array.from(this.favorites.values())
+    }
   },
   methods: {
     async  doSearch() {
@@ -18,12 +35,27 @@ const app = Vue.createApp({
         if (!response.ok) throw new Error("User not found")
         const data = await response.json()
         console.log(data)
-        this.result = true
+        this.result = data
       } catch(error) {
         this.error = error
       } finally {
         this.search = null
       }
+    },
+    addFavorite() {
+      this.favorites.set(this.result.id, this.result);
+      this.updateStorage();
+    },
+    removeFavorite() {
+      this.favorites.delete(this.result.id, this.result);
+      this.updateStorage();
+    },
+    showFavorite(favorite) {
+      this.result = favorite;
+    },
+    updateStorage()
+    {
+      window.localStorage.setItem('favorites', JSON.stringify(this.allFavorites))
     }
   }
 });
